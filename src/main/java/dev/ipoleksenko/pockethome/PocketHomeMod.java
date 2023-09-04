1e75368c77918a194988ff1d6526cc20193b1ccb
IPOleksenko <1ipooip1@gmail.com>
2023-09-04 14:10:44 +0300

    style: delete superfluous argument and imports

--- src/main/java/dev/ipoleksenko/pockethome/PocketHomeMod.java
+++ src/main/java/dev/ipoleksenko/pockethome/PocketHomeMod.java
@@ -2,12 +2,10 @@ package dev.ipoleksenko.pockethome;
 
 import dev.ipoleksenko.pockethome.event.EventPlayerJoin;
 import net.fabricmc.api.ModInitializer;
-import net.fabricmc.fabric.api.event.lifecycle.v1.ServerEntityEvents;
 import net.fabricmc.fabric.api.event.player.UseBlockCallback;
 import net.fabricmc.fabric.api.networking.v1.ServerPlayConnectionEvents;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
-import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.text.Text;
 import net.minecraft.util.ActionResult;
 import org.slf4j.Logger;
@@ -15,12 +13,9 @@ import org.slf4j.LoggerFactory;
 
 public final class PocketHomeMod implements ModInitializer {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(PocketHomeMod.class);
     @Override
     public void onInitialize() {
-        ServerPlayConnectionEvents.INIT.register((handler, server) -> {
-            EventPlayerJoin.send(handler, server);
-        });
+        ServerPlayConnectionEvents.INIT.register((handler, server) -> EventPlayerJoin.send(handler));
 
         UseBlockCallback.EVENT.register((player, world, hand, hitResult) -> {
             BlockState blockState = world.getBlockState(hitResult.getBlockPos());
--- src/main/java/dev/ipoleksenko/pockethome/event/EventPlayerJoin.java
+++ src/main/java/dev/ipoleksenko/pockethome/event/EventPlayerJoin.java
@@ -1,28 +1,25 @@
 package dev.ipoleksenko.pockethome.event;
 
-import net.fabricmc.fabric.api.networking.v1.ServerPlayConnectionEvents;
-import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.network.ServerPlayNetworkHandler;
 import net.minecraft.stat.Stats;
 import net.minecraft.text.Text;
 
 public class EventPlayerJoin {
     private static String messageFirstJoin = """
-                        Hello, %s
-                        I'm a PocketHome
-                        ---------------------------------------------
-                        What would join to your pocket home
-                        You need to use an ender chest while sneaking
-                        ---------------------------------------------
-                        What would swap time in your pocket home
-                        You need dream in it
-                        ---------------------------------------------
-                        What would swap weather in your pocket home
-                        You need use harem no jutsu!
-                        """;
+            Hello, %s
+            I'm a PocketHome
+            ---------------------------------------------
+            What would join to your pocket home
+            You need to use an ender chest while sneaking
+            ---------------------------------------------
+            What would swap time in your pocket home
+            You need dream in it
+            ---------------------------------------------
+            What would swap weather in your pocket home
+            You need use harem no jutsu!
+            """;
 
-    public static void send(ServerPlayNetworkHandler handler, MinecraftServer server) {
+    public static void send(ServerPlayNetworkHandler handler) {
         if (handler.player.getStatHandler().getStat(Stats.CUSTOM.getOrCreateStat(Stats.LEAVE_GAME)) == 0)
             handler.player.sendMessage(Text.of(String.format(messageFirstJoin, handler.player.getEntityName())));
     }

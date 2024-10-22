01b030bfca3a6791cc1826e6722c89bc1a251a8c
IPOleksenko <1ipooip1@gmail.com>
2024-10-22 22:39:46 +0300

    change start message

--- src/main/java/dev/ipoleksenko/pockethome/event/EventPlayerJoin.java
+++ src/main/java/dev/ipoleksenko/pockethome/event/EventPlayerJoin.java
@@ -11,12 +11,6 @@ public class EventPlayerJoin {
             ---------------------------------------------
             What would join to your pocket home
             You need to use an ender chest while sneaking
-            ---------------------------------------------
-            What would swap time in your pocket home
-            You need dream in it
-            ---------------------------------------------
-            What would swap weather in your pocket home
-            You need use harem no jutsu!
             """;
 
     public static void send(ServerPlayNetworkHandler handler) {
